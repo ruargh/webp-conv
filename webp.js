4223c8fd48e61852c7f7d74512ed4e0ed671d44c
Ruargh <ruargh@gmail.com>
2018-11-19 11:30:00 +0200

    test images jpeg 100

--- webp.js
+++ webp.js
@@ -1,17 +1,21 @@
-var imagemin = require("imagemin"),    // The imagemin module.
-  webp = require("imagemin-webp"),   // imagemin's WebP plugin.
-  outputFolder = "./img",            // Output folder
-  PNGImages = "./img/*.png",         // PNG images
-  JPEGImages = "./img/*.jpg";        // JPEG images
+var imagemin = require('imagemin'), // The imagemin module.
+  webp = require('imagemin-webp'), // imagemin's WebP plugin.
+  outputFolder = './img', // Output folder
+  PNGImages = './img/*.png', // PNG images
+  JPEGImages = './img/*.jpg'; // JPEG images
 
 imagemin([PNGImages], outputFolder, {
-  plugins: [webp({
+  plugins: [
+    webp({
       lossless: true // Losslessly encode images
-  })]
+    })
+  ]
 });
 
 imagemin([JPEGImages], outputFolder, {
-  plugins: [webp({
-    quality: 65 // Quality setting from 0 to 100
-  })]
+  plugins: [
+    webp({
+      quality: 100 // Quality setting from 0 to 100
+    })
+  ]
 });
